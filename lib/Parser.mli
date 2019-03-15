2ed43e140ef5bce57434c701f3b545b969792ac2
eddie <appositum@protonmail.com>
2019-03-14 23:13:24 -0300

    Remove parser interface (for now)

--- lib/Parser.mli
+++
@@ -1,6 +0,0 @@
-val pretty_token : token -> char
-val token_to_string : token -> string
-val is_alpha : char -> bool
-val tokenize : char list -> token list
-val unpack : string -> char list
-(* val pack : char list -> string *)
